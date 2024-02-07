f158118a321c271e93dcd5040cb60a5fae292a5b
Your Name <you@example.com>
2024-02-07 15:53:10 +0400

    Main

---
+++ Models/Feature.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EduHome.Models
+{
+    public class Feature
+    {
+        public int Id { get; set; }
+        [StringLength(maximumLength:50)]
+        public string Name { get; set; }
+        [StringLength(maximumLength:20)]
+        public string Value { get; set; }
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+using EduHome.DAL;
+using EduHome.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+}).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
+var app = builder.Build();
+app.UseStaticFiles();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseRouting();
+
+app.MapControllerRoute(
+    "Default",
+    "{controller=Home}/{action=Index}/{id?}");
+
+
+app.Run();
